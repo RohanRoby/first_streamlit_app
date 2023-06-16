fe337657f789d441a8ad5e6d061231b4430f102c
RohanRoby <58211572+RohanRoby@users.noreply.github.com>
2023-06-16 22:07:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('My Parents New healthy Dinner')
 streamlit.title('Breakfast Menu')
 streamlit.body('Omega 3 & Blueberry Oatmeal \
                 kale, Spinach and Rocket Smoothie\
-               Hard-Boiled Free-Range Egg'\)
+               Hard-Boiled Free-Range Egg')
